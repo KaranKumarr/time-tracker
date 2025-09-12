3ea850d00eda5473839bb0f9e79caa16bf2e132d
karankumarr <karankumar0334@gmail.com>
2025-09-12 15:36:49 +0200

    Categories which have exceeded their deadline will automatically be marked EXPIRED

--- src/main/java/org/karankumarr/timetracker/category/repository/CategoryRepository.java
+++ src/main/java/org/karankumarr/timetracker/category/repository/CategoryRepository.java
@@ -1,10 +1,14 @@
 package org.karankumarr.timetracker.category.repository;
 
+import jakarta.transaction.Transactional;
 import org.karankumarr.timetracker.category.dto.CategoryResponse;
 import org.karankumarr.timetracker.category.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface CategoryRepository extends JpaRepository<Category, Integer> {
@@ -17,5 +21,16 @@ public interface CategoryRepository extends JpaRepository<Category, Integer> {
             "GROUP BY c.id, c.name, c.description, c.goalHours, c.createdAt, c.deadline, c.status")
     List<CategoryResponse> findAllWithLoggedMinutes();
 
+
+
+    @Modifying
+    @Transactional
+    @Query("""
+    UPDATE Category c
+       SET c.status = org.karankumarr.timetracker.category.entity.GoalStatus.EXPIRED
+     WHERE c.deadline < :now
+       AND c.status = org.karankumarr.timetracker.category.entity.GoalStatus.ACTIVE
+""")
+    void markExpired(@Param("now") LocalDateTime now);
 
 }
